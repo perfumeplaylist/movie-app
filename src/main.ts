e521a1d197bfca58e6c2ad75683f8ed8f3dda07f
kim <perfumeplaylist.gmail.com>
2023-08-14 21:41:30 +0900

    refactor : 경로 다시 지정

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import router from '~/router/index.ts'
-import '~/main.scss'
+import router from './router/index.ts'
+import './main.scss'
 import App from './App.vue'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
+import HomePage from '../router/HomePage.vue'
+import MovieDetailView from '../view/MovieDetailView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '~/router/HomePage.vue'
-// import ModalView from '~/router/ModalView.vue'
-import MovieDetailView from '~/view/MovieDetailView.vue'
+
 export default createRouter({
   history: createWebHistory(),
   routes: [
